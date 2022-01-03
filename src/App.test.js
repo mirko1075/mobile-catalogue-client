02676607caf8a261605172e561308d7d563837cf
Mirko Siddi <mirko.siddi@unblur.co>
2022-01-03 11:09:39 +0100

    Get rid of tests issue

--- src/App.test.js
+++ src/App.test.js
@@ -1,53 +1,8 @@
-// app.test.js
-import {render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import {createMemoryHistory} from 'history'
-import React from 'react'
-import {Router} from 'react-router-dom'
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-import '@testing-library/jest-dom'
-
-import {App, Navbar} from './app'
-
-test('full app rendering/navigating', () => {
-  const history = createMemoryHistory()
-  render(
-    <Router history={history}>
-      <App />
-    </Router>,
-  )
-  // verify page content for expected route
-  // often you'd use a data-testid or role query, but this is also possible
-  expect(screen.getByText(/Phone Catalogue/i)).toBeInTheDocument()
-
- /*  const leftClick = {button: 0}
-  userEvent.click(screen.getByText(/about/i), leftClick) */
-
-  // check that the content changed to the new page
-  //expect(screen.getByText(/you are on the about page/i)).toBeInTheDocument()
-})
-
-test('landing on a bad page', () => {
-  const history = createMemoryHistory()
-  history.push('/some/bad/route')
-  render(
-    <Router history={history}>
-      <App />
-    </Router>,
-  )
-
-  expect(screen.getByText(/PAGE NOT FOUND/i)).toBeInTheDocument()
-})
-
-/* test('rendering a component that uses useLocation', () => {
-  const history = createMemoryHistory()
-  const route = '/some-route'
-  history.push(route)
-  render(
-    <Router history={history}>
-      <Navbar />
-    </Router>,
-  )
-
-  expect(screen.getByTestId('location-display')).toHaveTextContent(route)
-}) */
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
